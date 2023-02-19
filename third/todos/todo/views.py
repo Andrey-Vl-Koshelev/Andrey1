fc7a4228ae75cfa41ec5ee63c2103fa5e66d0477
Andrey <Koshelev-1969@mail.ru>
2023-02-19 15:35:33 +0400

    OOP

--- third/todos/todo/views.py
+++ third/todos/todo/views.py
@@ -6,6 +6,7 @@ from django.db import InternalError
 from .forms import TodoForm
 from .models import Todo
 from django.utils import timezone
+from django.contrib.auth.decorators import login_required
 
 
 def home(request):
@@ -42,17 +43,20 @@ def signupuser(request):
             return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Пароли не совпадают'})
 
 
+@login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
 
 
+@login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, date_completed__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
 
 
+@login_required
 def createtodo(request):
     if request.method == 'GET':
         return render(request, 'todo/createtodo.html', {'form': TodoForm()})
@@ -68,6 +72,7 @@ def createtodo(request):
                           {'form': TodoForm(), 'error': 'Переданы не верные данные. Попробуйте еще раз.'})
 
 
+@login_required
 def viewtodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk)
     if request.method == 'GET':
@@ -82,6 +87,7 @@ def viewtodo(request, todo_pk):
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Неверные данные'})
 
 
+@login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
@@ -90,6 +96,7 @@ def completetodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
@@ -97,6 +104,7 @@ def deletetodo(request, todo_pk):
         return redirect('currenttodos')
 
 
+@login_required
 def completedtodo(request):
     todos = Todo.objects.filter(user=request.user, date_completed__isnull=False).order_by('-date_completed')
-    return render(request, 'todo/completedtodo.html', {'todos':todos})
+    return render(request, 'todo/completedtodo.html', {'todos': todos})
