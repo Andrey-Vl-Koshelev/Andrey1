c219b8bc7f4a7ac85ddf8750b3e43b3eedc984c7
Andrey <Koshelev-1969@mail.ru>
2023-03-12 13:32:12 +0400

    OOP

--- fourth/devsearch/projects/forms.py
+++ fourth/devsearch/projects/forms.py
@@ -1,11 +1,28 @@
 from django.forms import ModelForm
-from .models import Project
+from .models import Project, Review
 from django import forms
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['value', 'body']
+
+        labels = {
+            'value': 'Place your vote',
+            'body': 'Add a comments with your vote'
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
 
 class ProjectForm(ModelForm):
     class Meta:
         model = Project
-        fields = ['title','featured_image', 'description', 'demo_link', 'source_link', 'tags']
+        fields = ['title', 'featured_image', 'description', 'demo_link', 'source_link', 'tags']
 
         widgets = {
             'tags': forms.CheckboxSelectMultiple(),
@@ -15,7 +32,7 @@ class ProjectForm(ModelForm):
         super().__init__(*args, **kwargs)
 
         for name, field in self.fields.items():
-            field.widget.attrs.update({'class':'input'})
+            field.widget.attrs.update({'class': 'input'})
 
-        # self.fields['title'].widget.attrs.update({'class':'input'})
-        # self.fields['description'].widget.attrs.update({'class':'input'})
+        # self.fields['title'].widget.attrs.update({'class': 'input'})
+        # self.fields['description'].widget.attrs.update({'class': 'input'})
